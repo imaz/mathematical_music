4688cea445f2420e9de666c72a29ab33b7621aaf
IMAZU Asami <neko.daisuki.070109@gmail.com>
2014-02-01 12:29:31 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusingMusic::Application.config.secret_key_base = '7fb89802aa9986888fbadc42c8fe1f5e6410e4b4903a7384b658d6584f59c5ade70f39967e34f727987ee6f89e42de88f68f555fb66aa24dce101907ebaaee9d'
